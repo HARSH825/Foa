fb306e89cd6a0c977bc30d367bad929890b69378
HARSH825 <harshchhallani3@gmail.com>
2025-05-29 17:52:23 +0530

    add oAuth

--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import passport from './auth/gAuth.js';
+import authRoutes from './routes/authRoutes.js';
+
 
 const app = express();
 dotenv.config();
+
+
+app.use(passport.initialize());
+// Routes
+app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
